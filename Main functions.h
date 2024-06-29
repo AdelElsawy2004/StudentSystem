98c421d092c63772ba8295af70ecace13aacddcc
Adel <adelmr721@gmail.com>
2024-06-29 07:48:04 +0300

    dividing the switch statement in the main function and putting it in a seperate file

--- Main functions.h
+++ Main functions.h
@@ -1,5 +1,10 @@
 #include <iostream>
+#include "Database Class.h"
 using namespace std ;
+Database db ;
+void switchPartTwo(int choice) ;
+void switchPartThree(int choice) ;
+void switchPartFour(int choice) ;
 void options(){
 	cout << "\t\t\t\t\t\t\t  Welcome to our Student System\n\n\n" ;
 		cout << "If you want to ADD STUDENT press 1\n\n" ;
@@ -24,4 +29,61 @@ void endMessage(){
 	cout << "\n\nThanks for using our system\n" ;
 	cout << "If you want to exit press 0\n" ;
 	cout << "If you want to return to the start menu press any number\n" ;
+}
+void switchPartOne(int choice){
+	switch (choice){
+		case 0 : return ;
+		case 1 :
+			db.addStudent() ; break ;
+		case 2 :
+			db.addCourse() ; break ;
+		case 3 :
+			db.searchStudent() ; break ;
+		case 4 :
+			db.searchCourse() ; break ;
+		default :
+			switchPartTwo(choice) ;
+	}
+}
+void switchPartTwo(int choice){
+	switch (choice){
+		case 5 :
+			db.enrollStudent() ; break ;
+		case 6 :
+			db.showStudentEnrolledCourses() ; break ;
+		case 7 :
+			db.addGrade() ; break ;
+		case 8 :
+			db.getStudentGrades() ; break ;
+		default :
+			switchPartThree(choice) ;
+	}
+}
+void switchPartThree(int choice){
+	switch (choice){
+		case 9 :
+			db.updateGrade() ; break ;
+		case 10 :
+			db.removeStudent() ; break ;
+		case 11 :
+			db.removeCourse() ; break ;
+		case 12 :
+			db.showCourseEnrollments() ; break ;
+		default :
+			switchPartFour(choice) ;
+	}
+}
+void switchPartFour(int choice){
+	switch (choice){
+		case 13 :
+			db.reportOfAllStudents() ; break ;
+		case 14 :
+			db.reportOfAllCourses() ; break ;
+		case 15 :
+			db.modifyStudent() ; break ;
+		case 16 :
+			db.modifyCourse() ; break ;
+		default :
+			cout << "Invalid choice\n" ;
+	}
 }
